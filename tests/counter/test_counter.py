eb3dedecca42ef589736d49f5b8911b8fba790d3
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-01-29 19:04:24 -0300

    requeriment_01

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    path = "data/jobs.csv"
+    word_python = "Python"
+    word_java = "java"
+
+    count_python = count_ocurrences(path, word_python)
+    count_java = count_ocurrences(path, word_java)
+
+    assert count_python == 1639
+    assert count_java == 676

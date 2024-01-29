30039a275a2acb5de47f47b926b9db16d0fcb045
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-01-29 20:13:21 -0300

    requeriment_02

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,15 +1,27 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path) -> List[Dict]:
+        with open(path, "r", encoding="utf-8") as filejobs:
+            jobs_reader = csv.DictReader(
+                filejobs, delimiter=",", quotechar='"'
+            )
+            self.jobs_list = list(jobs_reader)
+
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
+
+
+process_jobs = ProcessJobs()
+dados = process_jobs.read("data/jobs.csv")
+print(dados)

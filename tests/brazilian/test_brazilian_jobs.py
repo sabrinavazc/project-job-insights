2edc0694591709f709241268d2d9b30b0f9128eb
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-01-30 17:34:57 -0300

    requeriment_10_finished_project

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,20 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    path = "tests/mocks/brazilians_jobs.csv"
+
+    result = read_brazilian_file(path)
+
+    assert isinstance(result, list)
+
+    assert len(result) > 0
+
+    for job in result:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
+
+        assert "titulo" not in job
+        assert "salario" not in job
+        assert "tipo" not in job

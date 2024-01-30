340c5a68cb50386ff4f7620da1abe92c1b47c8e5
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2024-01-30 15:03:40 -0300

    requeriment_05

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,9 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        unique_industries_type = set(
+            industry["industry"]
+            for industry in self.jobs_list
+            if industry.get("industry")
+        )
+        return sorted(list(unique_industries_type))
